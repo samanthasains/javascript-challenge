2575e9857d380c95e3fc4d8cac0adad5e10298ee
Samantha Sainsbury <samanthasains@gmail.com>
2021-01-02 15:18:46 -0600

    added code into app.js to write data to table

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Use d3 to update each cell's text with ufo report values
+data.forEach(function(ufoFinder) {
+  console.log(ufoFinder);
+  var row = tbody.append("tr");
+  Object.entries(ufoFinder).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value in the ufo finder object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
