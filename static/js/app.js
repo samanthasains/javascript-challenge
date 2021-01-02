7ad79c4ecd4796234a2eb7c2495de6b07b5f3aa6
Samantha Sainsbury <samanthasains@gmail.com>
2021-01-02 16:06:32 -0600

    Added button click and date filter, debugging empty set for filtered data

--- static/js/app.js
+++ static/js/app.js
@@ -8,12 +8,47 @@ var tbody = d3.select("tbody");
 
 // Use d3 to update each cell's text with ufo report values
 data.forEach(function(ufoFinder) {
-  console.log(ufoFinder);
+//   console.log(ufoFinder);
   var row = tbody.append("tr");
   Object.entries(ufoFinder).forEach(function([key, value]) {
-    console.log(key, value);
+    // console.log(key, value);
     // Append a cell to the row for each value in the ufo finder object
     var cell = row.append("td");
     cell.text(value);
   });
+});
+
+// Select the submit button
+var submit = d3.select("#filter-btn");
+
+//Click event of datetime filter
+submit.on("click", function() {
+
+// Remove existing table
+d3.select("tbody").html("");
+
+// Prevent the page from refreshing
+d3.event.preventDefault();
+
+// Get the value property of the input element
+var dateTime = d3.select("#datetime").property("value");
+console.log(dateTime);
+
+// Filter data
+var filteredData = tableData.filter(record => record.dateTime === dateTime);
+console.log(filteredData);
+
+// Display filtered data
+filteredData.forEach(function(ufoFinder) {
+    console.log(ufoFinder)
+    var row = tbody.append("tr");
+    Object.entries(ufoFinder).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value in the ufo finder object
+      var cell = row.append("td");
+      cell.text(value);
+});
+
+});
+
 });
